c687c1d759d87116e4ec4368cec10dbc9e9a9338
oldixi <oldixi@yandex.ru>
2025-11-10 19:43:21 +0300

    feat: notifications is up

--- notifications/src/main/java/ru/yandex/notifications/NotificationsApplication.java
+++ notifications/src/main/java/ru/yandex/notifications/NotificationsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableConfigurationProperties
 public class NotificationsApplication {
 	public static void main(String[] args) {
